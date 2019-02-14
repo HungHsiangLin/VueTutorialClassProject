87d7bb7ad96cc754de2bd9bcb792469b62762a59
ArmorTiger <17711129+HungHsiangLin@users.noreply.github.com>
2019-02-14 17:30:49 +0800

    Update README.md

---
+++ Tutorial/Tutorial1/README.md
@@ -0,0 +1,5 @@
+# VUE課程_Project1
+V-on
+v-bind
+v-once
+v-html
---
+++ Tutorial/Tutorial1/app.js
@@ -0,0 +1,18 @@
+﻿new Vue({
+	/* 選取控制的element */
+  el: '#app',
+  /* 資料存放 */
+  data: {
+    title: '測試',
+    link: 'http://google.com',
+    finishedLink: '<a href="http://google.com">Google</a>'
+  },
+  methods: {
+    changeTitle: function(event) {
+      this.title = event.target.value;
+    },
+    sayHello: function() {
+      return this.title
+    }
+  }
+});
